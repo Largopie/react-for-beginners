8096110e0de96473bbedcda846085831eb506d43
Largopie <106071687+Largopie@users.noreply.github.com>
2023-01-25 16:40:23 +0900

    Movie App Part One

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ToDoList from './todolist';
-import CoinTracker from './coinTracker';
+// import ToDoList from './todolist';
+// import CoinTracker from './coinTracker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <CoinTracker />
+    <App />
 );
