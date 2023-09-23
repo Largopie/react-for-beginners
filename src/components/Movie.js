bd22b1a6b6dce60ec4ccfa80f6580ac2b0faf8a3
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-23 17:56:17 +0900

    ui: 영화 리스트 grid layout 구성

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,37 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+
+const Image = styled.img`
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    max-width: 100px;
+  }
+`
+
+const Summary = styled.div`
+  height: 40px;
+  overflow: scroll;
+`
+
 const Movie = ({ id, coverImg, title, summary, genres}) => {
   return (
-    <div>
-      <img alt="coverImg" src={coverImg} />
+    <Container>
+      <Image alt="coverImg" src={coverImg} />
       <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-      <p>{summary}</p>
+      <Summary>{summary}</Summary>
       <ul>
         {genres.map((genre) => <li key={genre}>{genre}</li>)}
       </ul>
-    </div>
+    </Container>
   );
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,39 +3,48 @@ import Movie from '../components/Movie';
 import styled from 'styled-components';
 
 const MainContainer = styled.div`
+  width: 100vw;
+  padding: 30px;
+`
+
+const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: repeat(3, 1fr);
   align-items: center;
   justify-content: center;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
 
 function Home() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-                `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-            )
-        ).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        getMovies();
-    }, []);
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-    console.log(movies);
+  console.log(movies);
 
-    return (
-        <MainContainer>
-            {loading ? <h1>loading...</h1> : <div>{movies.map((movie) =>
-                <Movie summary={movie.summary} id={movie.id} key={movie.id} coverImg={movie.large_cover_image} title={movie.title} genres={movie.genres} />
-            )}</div>}
-        </MainContainer>
-    );
+  return (
+    <MainContainer>
+      {loading ? <h1>loading...</h1> : <GridContainer>{movies.map((movie) =>
+        <Movie summary={movie.summary} id={movie.id} key={movie.id} coverImg={movie.large_cover_image} title={movie.title} genres={movie.genres} />
+      )}</GridContainer>}
+    </MainContainer>
+  );
 }
 
 export default Home;
