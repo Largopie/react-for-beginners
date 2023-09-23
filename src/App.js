e07177fe1c5a430b19428f562d714399d649863a
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-23 17:21:33 +0900

    ui: css 초기화

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
+import GlobalStyles from './globalStyles';
+
 
 function App() {
   return (
     <Router>
+      <GlobalStyles />
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
